f875927fac1f8dec255086fc105947070fee88c5
klueless-io <david@ideasmen.com.au>
2021-03-23 11:04:58 +1100

    first commit

---
+++ lib/k_builder/watch.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "watch/version"
+
+module KBuilder
+  module Watch
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/k_builder/watch/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module KBuilder
+  module Watch
+    VERSION = "0.1.0"
+  end
+end
